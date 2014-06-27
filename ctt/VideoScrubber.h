860e6485c35a2e9340b7ae76d96c8123463d9d23
Kakistocrtor <jonaslkraemer@gmail.com>
2014-06-27 17:29:25 +0200

    Corrected some codegeneration mistakes in VideoScrubber and added basic comments.

--- ctt/VideoScrubber.h
+++ ctt/VideoScrubber.h
@@ -11,20 +11,66 @@
 #include "VideoMetadata.h"
 #include "Memento.h"
 
+/**
+ * A VideoScrubber is associated with a video and able to request frames from this video to save one of them internally.
+ * It makes this frame available to other objects and notifies them via Qt's signal and slot mechanism when this frame is replaced.
+ */
 class VideoScrubber : public Savable, public Observable {
+
 public:
-	virtual slot jumpToFrameNr(int frameNumber) = 0;
-	Frame getCurrentFrame();
-	void VideoScrubber(Video video);
-	void VideoScrubber(Video video, int frameNumber);
-	VideoMetadata getVideoMetadata();
-	Video getVideo();
-	Memento getMemento();
-	void restore(Memento memento);
-	Savable* getDummy();
+    /**
+     * Creates a new VideoScrubber for the submitted video. Initially the Scrubber holds the first frame of the video.
+     *
+     * @param video the scrubber will use this video to get frames
+     */
+    VideoScrubber(Video video);
+
+    /**
+     * Creates a new VideoScrubber for the submitted video, initially holding the frame with the submitted number.
+     *
+     * @param video video the scrubber will use this video to get frames
+     * @param frameNumber the scrubber will initially hold the frame with this number
+     */
+    VideoScrubber(Video video, int frameNumber);
+
+    /**
+     * Gets the metadata of the Video this VideoScrubber is associated with.
+     *
+     * @return VideoMetadata the metadata of the Video this VideoScrubber is associated with
+     */
+    VideoMetadata getVideoMetadata();
+
+    /**
+     * Gets the Video this VideoScrubber is associated with.
+     *
+     * @return Video the Video this VideoScrubber is associated with.
+     */
+    Video getVideo();
+
+    /**
+     * Gets the frame currently held by the scrubber.
+     *
+     * @return Frame the frame currently held by the scrubber
+     */
+    Frame getCurrentFrame();
+
+    Memento getMemento();
+
+    void restore(Memento memento);
+
+    Savable* getDummy();
+
+public slots:
+    /**
+     * The scrubber loads the frame with the provided frameNumber from the Video, saves it (overwriting the previously saved frame) and signals the change to connected slots.
+     *
+     * @param frameNumber the number of the frame wich will be requested from the video
+     */
+    void jumpToFrameNr(int frameNumber) = 0;
+
 private:
-	Video *video;
-	Frame *currentFrame;
+    Video *video; /**< The scrubber gets frames and metadata from this video */
+    Frame *currentFrame; /**< This is the frame currently held by the scrubber */
 };
 
 #endif  //_VIDEOSCRUBBER_H
