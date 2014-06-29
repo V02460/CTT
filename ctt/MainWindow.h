a23401c75ee57151765eb38696a432c5fe88e39e
ugdsc <ugdsc@student.kit.edu>
2014-06-29 20:49:04 +0200

    Comment MainWindow

--- ctt/MainWindow.h
+++ ctt/MainWindow.h
@@ -5,16 +5,30 @@
 #define _MAINWINDOW_H
 
 #include "Observable.h"
+#include "Observer.h"
 #include "ViewType.h"
 #include "ViewState.h"
+#include "ProcessingWidget.h"
+#include "AnalysingWidget.h"
+#include <QMenuBar>
+#include <QMainWindow>
 
-class MainWindow : public Observable {
-public:
-	signal menuItemViewStateChanged(ViewType newView);
-	slot viewStateChanged();
+/**
+ *	The MainWindow unites all interactable components which represent the whole functionallity provided by the model.
+ */
+class MainWindow : public Observable , public Observer, public QMainWindow {
+signals:
+	/**
+	 *	This signal is emitted when the view state is changed
+	 *
+	 *	@param newView The identifier of the view state that should be set as active
+	 */
+	void menuItemViewStateChanged(ViewType newView);
 private:
-	ViewState viewState;
-	QMenuBar menu;
+	ViewState viewState; /**< The current state the whole GUI is in */
+	QMenuBar menu; /**< The menu bar which holds all menu items */
+	ProcessingWidget processingWidget; /**< The widget to represent the processing view */
+	AnalysingWidget analysingWidget; /**< The widget to represent the analysing view */
 };
 
 #endif  //_MAINWINDOW_H
