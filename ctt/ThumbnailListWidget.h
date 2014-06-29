4d5069fd1aa235a8859a3c1bd1bf3f25ddd79105
ugdsc <ugdsc@student.kit.edu>
2014-06-29 18:28:44 +0200

    Comment ThumbnailListWidget

--- ctt/ThumbnailListWidget.h
+++ ctt/ThumbnailListWidget.h
@@ -5,16 +5,43 @@
 #define _THUMBNAILLISTWIDGET_H
 
 #include "Observer.h"
+#include "Video.h"
+#include <QWidget>
+#include <QPushButton>
+#include <QList>
+#include <QDir>
 
-class ThumbnailListWidget : public Observer {
+/**
+ *	The TumbnailListWidget displays a set of videos by its thumbnails and provides the interactiable components to set one or more videos
+ *	active, to add videos to and to remove videos from the program
+ */
+class ThumbnailListWidget : public Observer, public QWidget {
 public:
-	void ThumbnailListWidget(SaveableList<Video> filteredVideos, int selectableCount);
-	signal videoAdded(QDir path);
-	signal videoRemoved(int index);
+	/**
+	 *	Creates a new TumbnailListWidget with a SavableList of filteredVideos which is diplayed by this widget.
+	 *	The ThumbnailListWidget registers itself at the SaveableList to get notified, when the currently managed videos change
+	 *
+	 *	@param filteredVideos The list of videos which should be displayed by this widget
+	 *	@param selectableCount The number of video which can be activated simultaniously
+	 */
+	ThumbnailListWidget(SaveableList<Video> filteredVideos, int selectableCount);
+signals:
+	/**
+	 *	This signal is emitted when a new video is added to the program via the addVideo button
+	 *
+	 *	@param path The file path that leads to the video file
+	 */
+	void videoAdded(QDir path);
+	/**
+	 *	This signal is emitted when an exisitng video is removed from the program
+	 *
+	 *	@param index The index in the filteredVideos list of the video which should be removed
+	 */
+	void videoRemoved(int index);
 private:
-	List<QPushButton> thumbnailList;
-	QPushButton btnAddVideo;
-	SaveableList<Video> filteredVideos;
+	QList<QPushButton> thumbnailList; /**< The list of buttons with the thumbnails of the specific videos as icons */
+	QPushButton btnAddVideo; /**< The button to add a new video */
+	SaveableList<Video> filteredVideos; /**< The list of filteredVideo which is needed for the thumbnail generation/
 };
 
 #endif  //_THUMBNAILLISTWIDGET_H
