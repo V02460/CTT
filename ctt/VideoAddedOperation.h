83719891948685a995a3c846db1ecc9bc9761501
fernel <n.fernengel@gmail.com>
2014-06-29 23:58:15 +0200

    Comments on Operations: VideoAdded

--- ctt/VideoAddedOperation.h
+++ ctt/VideoAddedOperation.h
@@ -7,10 +7,27 @@
 #include "VideoListOperation.h"
 #include "Video.h"
 
+/**
+*	The VideoAddedOperation is providing functionality for doing and undoing adding a Video to a VideoList.
+**/
 class VideoAddedOperation : public VideoListOperation {
 public:
-	void VideoAddedOperation(Video video, SavableList<Video> videoList);
-	void do();
+
+	/**
+	*	Constructs an Operation that is responsible for adding a specified Video to a VideoList.
+	*	@param video The video which should be added to a list of videos.
+	*	@param videoList The list to which a certain video should be added.
+	**/
+	VideoAddedOperation(Video video, SaveableList<Video> videoList);
+
+	/**
+	*	Manages requests to add a Video to a VideoList.
+	**/
+	void doOperation();
+
+	/**
+	*	Manages requests to undo adding a Video to a VideoList.
+	**/
 	void undo();
 };
 
