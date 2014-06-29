4191d4f53fe02b1e1b22d9f2040617c21a6360ba
Kakistocrtor <jonaslkraemer@gmail.com>
2014-06-29 10:13:30 +0200

    Added merge functions to IntegerInterval.

--- ctt/IntegerInterval.h
+++ ctt/IntegerInterval.h
@@ -7,12 +7,14 @@
 
 class IntegerInterval {
 public:
-	void IntegerInterval(int a, int b);
+	IntegerInterval(int a, int b);
 	bool contains(int x);
 	int getStart();
 	int getEnd();
 	void setStart(int start);
 	void setEnd(int end);
+	bool canMergeWith(IntegerInterval interval);
+	void mergeWith(IntegerInterval interval);
 private:
 	int start;
 	int end;
