c6c02ed4a7c1799cc4403ed3b1600b394a9e2179
fernel <n.fernengel@gmail.com>
2014-06-29 23:52:18 +0200

    Comments on OperationList

--- ctt/OperationList.h
+++ ctt/OperationList.h
@@ -6,14 +6,46 @@
 
 #include "Operation.h"
 
+/**
+*	The OperationList contains a list with every Operation executed since the last load of the Project and the functionality to do, undo and redo those Operations.
+**/
 class OperationList {
 public:
-	void do(Operation operation);
-	void undo();
-	void redo();
+
+	/**
+	*	Manages doing the given Operation.
+	*	@param operation The Operation that should be done.
+	**/
+	void doOperation(Operation operation);
+
+	/**
+	*	Manages undoing the Operation that has been done previously.
+	**/
+	void undoOperation();
+
+	/**
+	*	Manages redoing the Operation that has been undone previously.
+	**/
+	void redoOperation();
+
+	/**
+	*	Sets the last state of the Project which has been saved.
+	**/
 	void setLastSavedState();
-	bool hasSavableChanges();
+
+	/**
+	*	Determines whether there are actual changes since the last saved state.
+	**/
+	bool hasSaveableChanges();
+
+	/**
+	*	Determines whether an Operation has been done previously, making it possible to execute undoOperation.
+	**/
 	bool canUndo();
+
+	/**
+	*	Determines whether an Operation has been undone previously, making it possible to execute redoOperation.
+	**/
 	bool canRedo();
 private:
 	Operation *currentOperation;
