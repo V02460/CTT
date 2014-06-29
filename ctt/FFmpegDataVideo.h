aee6680de64fbfcb66664f274def5827c4621a5e
Kakistocrtor <jonaslkraemer@gmail.com>
2014-06-29 18:43:25 +0200

    Added OGL context Parameter to constructor of filevideos.

--- ctt/FFmpegDataVideo.h
+++ ctt/FFmpegDataVideo.h
@@ -17,10 +17,11 @@ public:
 	/**
 	 * Creates a new FFmpegDataVideo representing the mpeg-2, mpeg-4 part 2 or mpeg-4 part 10 video at the submitted location in the file system.
 	 * @param path the path to the video file which will be loaded
+	 * @param context the context in which the video creates its frames
 	 * @throws IOException if the file at the submitted location can't be accessed.
 	 * @throws InvalidArgumentException if the file at the submitted location isn't a valid mpeg-2, mpeg-4 part 2 or mpeg-4 part 10 video file
 	 */
-	FFmpegDataVideo(QDir path);
+	FFmpegDataVideo(QDir path, QOpenGLContext context);
 
     VideoMetadata getMetadata();
 
--- ctt/YUVDataVideo.h
+++ ctt/YUVDataVideo.h
@@ -19,10 +19,11 @@ public:
 	* Creates a new YUVDataVideo representing the YUV 4:4:4 or YUV 4:2:2 video at the submitted location in the file system.
 	* @param videoFile the path to the video file which will be loaded
 	* @param metadataFile the path to the file containing the metadata for the video file
+	* @param context the context in which the video creates its frames
 	* @throws IOException if the file at the submitted location can't be accessed.
 	* @throws InvalidArgumentException if the files at the submitted locations aren't valid YUV 4:4:4 or YUV 4:2:2 video or metadata files
 	*/
-	YUVDataVideo(QDir videoFile, QDir metadataFile);
+	YUVDataVideo(QDir videoFile, QDir metadataFile, QOpenGLContext context);
 
     VideoMetadata getMetadata();
 
