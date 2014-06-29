fc00f6a68e5752760f8955b5df960b39b941f774
ugdsc <ugdsc@student.kit.edu>
2014-06-29 17:17:18 +0200

    Comment FrameMetadataWidget

--- ctt/FrameMetadataWidget.h
+++ ctt/FrameMetadataWidget.h
@@ -5,10 +5,23 @@
 #define _FRAMEMETADATAWIDGET_H
 
 #include "Observer.h"
+#include "VideoScrubber.h"
 
+/**
+ *	The FrameMetadataWidget displays the metadata information provided by a frame. Therefore it registers itself at a VideoScrubber to
+ *	recieve the metadata for the current frame
+ */
 class FrameMetadataWidget : public Observer {
 public:
+	/**
+	 *	Creates a new FrameMetadataWidget which holds a VideoScrubber from where it gets the metadata for the current frame
+	 *
+	 *	@param scrubber The VideoScrubber to regiser at and which provides the metadata
+	 */
+	FrameMetadataWidget(VideoScrubber scrubber);
 
+private:
+	VideoScrubber scrubber; 
 };
 
 #endif  //_FRAMEMETADATAWIDGET_H
