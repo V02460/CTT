16a3931e983dbc06500bca9da651334ab310d08a
ugdsc <ugdsc@student.kit.edu>
2014-06-29 17:09:29 +0200

    Comment HistogramWidget

--- ctt/HistogramWidget.h
+++ ctt/HistogramWidget.h
@@ -5,12 +5,28 @@
 #define _HISTOGRAMWIDGET_H
 
 #include "Observer.h"
+#include "VideoScrubber.h"
 
+/**
+ *	The HistogramWidget displays different histograms for a video frame. Therefore it registeres itself at a VideoScrubber to recieve
+ *	the histograms
+ */
 class HistogramWidget : public Observer {
 public:
+	/**
+	 *	Creates a new HistogramWidget with a corresponding VideoScrubber from where it gets the histograms which are displayed
+	 *
+	 *	@param scrubber The VideoScrubber to register at and which provides the histograms
+	 */
+	HistogramWidget(VideoScrubber scrubber);
+	/**
+	 *	Shows the next histogram.
+	 *	So for example the widget switches from the histogram for reds to the one for greens
+	 */
 	void next();
 private:
 	void paint();
+	VideoScrubber scrubber;
 };
 
 #endif  //_HISTOGRAMWIDGET_H
