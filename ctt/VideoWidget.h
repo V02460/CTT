fe2265f8ee6f8f2753fd1750507d47819d5d08d2
ugdsc <ugdsc@student.kit.edu>
2014-06-29 15:37:23 +0200

    Commented VideoWidget

--- ctt/VideoWidget.h
+++ ctt/VideoWidget.h
@@ -4,14 +4,32 @@
 #if !defined(_VIDEOWIDGET_H)
 #define _VIDEOWIDGET_H
 
-#include "QWidget.h"
 #include "Observer.h"
 #include "VideoScrubber.h"
+#include <QWidget>
 
+/**
+ * The VideoWidget is responsible for showing a concrete video frame. Therefore it registers itself at a VideoScrubber
+ * and diplays the frame provided by it
+ */
 class VideoWidget : public QWidget, public Observer {
 public:
-	void VideoWidget(VideoScrubber scrubber);
+	/**
+	 * Recieves a VideoScrubber to register at and to save it internally
+	 *
+	 *	@param scubber The VideoScrubber to register at and which provided the video frames
+	 */
+	VideoWidget(VideoScrubber scrubber);
+
+	/**
+	 *	Returns the VideoScrubber at which the VideoWidget is registered and where the VideoWidget recieves its frames from
+	 *
+	 *	@return The VideoScrubber which holds the current frame
+	 */
 	VideoScrubber getScrubber();
+
+private:
+	VideoScrubber scrubber; /**< The scrubber at which the VideoWidget is registered */
 };
 
 #endif  //_VIDEOWIDGET_H
