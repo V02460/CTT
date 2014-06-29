edec433e87700a2a75393b3f3e162b6dec739166
Kakistocrtor <jonaslkraemer@gmail.com>
2014-06-29 19:38:14 +0200

    Added a constructor for Surface.

--- ctt/Surface.h
+++ ctt/Surface.h
@@ -8,8 +8,13 @@
 class Surface
 {
 public:
-
-	Surface();
+	/**
+	 * Creates a new Surface from the submitted texture and the submitted context.
+	 * WARNING: It can't be ensured that the submitted texture is accessible from the submitted context! If the caller wants an operative Surface, it's up to him to make that sure!
+	 * @param glContext an OpenGL context, from which the texture is accessible
+	 * @param glTexture the OpenGL texture
+	 */
+	Surface(QOpenGLContext glContext, QOpenGLTexture glTexture);
 
 	/**
 	* Gets the texture containing the image data of the frame.
