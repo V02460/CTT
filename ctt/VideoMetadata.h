39de767a4232ff219e5f569c7b3f8fac8f487450
Kakistocrtor <jonaslkraemer@gmail.com>
2014-06-29 15:08:30 +0200

    Added length of the Video to VideoMetadata.

--- ctt/VideoMetadata.h
+++ ctt/VideoMetadata.h
@@ -7,11 +7,13 @@
 
 class VideoMetadata {
 public:
-	void VideoMetadata(QSize size, double fps, QMap additionalData);
+	VideoMetadata(QSize size, double fps, unsigned int length, QMap<QString, QString> additionalData);
+	VideoMetadata(QSize size, double fps, unsigned int length);
 	bool hasData(QString key);
 	bool getData(QString key);
 	QSize getSize();
 	double getFPS();
+	unsigned int getLength();
 private:
 	QSize size;
 	double fps;
