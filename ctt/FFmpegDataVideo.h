f106caa1bb65ad6d40980a904907177135ce28f0
Kakistocrtor <jonaslkraemer@gmail.com>
2014-06-29 18:03:51 +0200

    Fixed and commented YUV and ffmpeg videos.

--- ctt/FFmpegDataVideo.h
+++ ctt/FFmpegDataVideo.h
@@ -7,14 +7,45 @@
 #include "Video.h"
 #include "Frame.h"
 #include "Memento.h"
+#include <QPath>
 
+/**
+ * A Video read from a mpeg-2, mpeg-4 part 2 or mpeg-4 part 10 video file.
+ */
 class FFmpegDataVideo : public Video {
 public:
-	VideoMetadata getMetadata();
-	Frame getFrame(int frameNumber);
-	Memento getMemento();
-	void restore(Memento memento);
-	Saveable* getDummy();
+	/**
+	 * Creates a new FFmpegDataVideo representing the mpeg-2, mpeg-4 part 2 or mpeg-4 part 10 video at the submitted location in the file system.
+	 * @param path the path to the video file which will be loaded
+	 * @throws IOException if the file at the submitted location can't be accessed.
+	 * @throws InvalidArgumentException if the file at the submitted location isn't a valid mpeg-2, mpeg-4 part 2 or mpeg-4 part 10 video file
+	 */
+	FFmpegDataVideo(QDir path);
+
+	/**
+	 * Returns the path to the video file this Video loads its frames from.
+	 * @return QDir the path to the video file this Video loads its frames from
+	 */
+	QDir getPath();
+
+    VideoMetadata getMetadata();
+
+    Frame getFrame(int frameNumber);
+
+    Memento getMemento();
+
+    void restore(Memento memento);
+
+    Saveable* getDummy();
+
+private:
+	/**
+	 * Creates a dummy FFmpegDataVideo.
+	 */
+	FFmpegDataVideo(QDir path);
+
+	QDir path; /**< the path to the video file this Video loads its frames from. */
+	VideoMetadata metadata; /**< metadata valid for the whole video. */
 };
 
 #endif  //_FFMPEGDATAVIDEO_H
--- ctt/YUVDataVideo.h
+++ ctt/YUVDataVideo.h
@@ -8,13 +8,39 @@
 #include "Frame.h"
 #include "Memento.h"
 
+/**
+ * A Video read from a YUV 4:4:4 or YUV 4:2:2 video file.
+ *
+ */
 class YUVDataVideo : public Video {
 public:
-	VideoMetadata getMetadata();
-	Frame getFrame(int frameNumber);
-	Memento getMemento();
-	void restore(Memento memento);
-	Saveable* getDummy();
+	/**
+	* Creates a new YUVDataVideo representing the YUV 4:4:4 or YUV 4:2:2 video at the submitted location in the file system.
+	* @param videoFile the path to the video file which will be loaded
+	* @param metadataFile the path to the file containing the metadata for the video file
+	* @throws IOException if the file at the submitted location can't be accessed.
+	* @throws InvalidArgumentException if the files at the submitted locations aren't valid YUV 4:4:4 or YUV 4:2:2 video or metadata files
+	*/
+	YUVDataVideo(QDir videoFile, QDir metadataFile);
+
+    VideoMetadata getMetadata();
+
+    Frame getFrame(int frameNumber);
+
+    Memento getMemento();
+
+    void restore(Memento memento);
+
+    Saveable* getDummy();
+
+private:
+	/**
+	* Creates a dummy YUVDataVideo.
+	*/
+	YUVDataVideo(QDir path);
+
+	QDir path; /**< the path to the video file this Video loads its frames from. */
+	VideoMetadata metadata; /**< metadata valid for the whole video. */
 };
 
 #endif  //_YUVDATAVIDEO_H
