6cd376718a83c3194a96448130e0aa62d3bb1957
Kakistocrtor <jonaslkraemer@gmail.com>
2014-06-30 10:58:33 +0200

    Fixed and Commented Observable.

--- ctt/Observable.h
+++ ctt/Observable.h
@@ -5,12 +5,34 @@
 #define _OBSERVABLE_H
 
 #include "Observer.h"
+#include "QObject"
 
-class Observable {
+/**
+ * An Object that Observers can subscribe to, to be notified when the state of the Observable changes.
+ *
+ */
+class Observable: public QObject {
 public:
-	signal changed();
-	void subscribe(Observer observer);
-	void unsubscribe(Observer observer);
+    /**
+     * Subscribes the submitted Observer to the Observable, i.e. the Observer will be notified when the state of the Observable changes.
+     *
+     * @param observer this Observer will be subscribed to the Observable.
+     */
+    void subscribe(Observer observer);
+
+    /**
+     * Unsubscribes the submitted Observer from the Observable, i.e. the Observer will not/no longer be notified when the state of the Observable changes.
+     *
+     * @param observer this Observer will be unsubscribed from the Observable.
+     */
+    void unsubscribe(Observer observer);
+
+signals:
+    /**
+     * this signal will be emitted when the state of the Observable changes.
+     *
+     */
+    void changed();
 };
 
 #endif  //_OBSERVABLE_H
