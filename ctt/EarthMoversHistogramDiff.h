747a159bf34f85611c224978309c991861faddb6
Kakistocrtor <jonaslkraemer@gmail.com>
2014-06-30 12:49:30 +0200

    Comments EarthMoversHistogramDiff and adds constructor.

--- ctt/EarthMoversHistogramDiff.h
+++ ctt/EarthMoversHistogramDiff.h
@@ -11,17 +11,33 @@
 namespace model {
 namespace difference {
 
+/**
+ * Calculates distances between frames of Videos using earth mover's distance on their channel intensity histograms.
+ */
+
 class EarthMoversHistogramDiff : public FrameDiff {
 public:
-	typedef QScopedPointer<EarthMoversHistogramDiff> uptr;
-	typedef QSharedPointer<EarthMoversHistogramDiff> sptr;
-	typedef QWeakPointer<EarthMoversHistogramDiff> wptr;
+    typedef QScopedPointer<EarthMoversHistogramDiff> uptr;
+    typedef QSharedPointer<EarthMoversHistogramDiff> sptr;
+    typedef QWeakPointer<EarthMoversHistogramDiff> wptr;
 
-	double getDif(int frameNr);
-	void FrameDif(Video video1, Video video2);
-	Memento getMemento();
-	void restore(Memento memento);
-	Saveable* getDummy();
+	/**
+	 * Creates a new EarthMoversHistogramDiff using the histograms of the submitted type to calculate earth mover's distances between frames of the two submitted videos.
+	 * @param type the type of histogram that will be used for the calculations
+	 * @param video1 the first of the two videos the EarthMoversHistogramDiff will compare 
+	 * @param video2 the second of the two videos the EarthMoversHistogramDiff will compare
+	 */
+	EarthMoversHistogramDiff(model::frame::histogram::HistogramType type, model::video::Video video1, model::video::Video video2);
+
+    double getDif(int frameNr);
+
+    void FrameDif(Video video1, Video video2);
+
+    model::project::Memento getMemento();
+
+    void restore(model::project::Memento memento);
+
+    model::project::Saveable* getDummy();
 };
 
 }  // namespace difference
