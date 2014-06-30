db01e577ffd9fada97e1c8cff0803a97c0d8b9a8
fernel <n.fernengel@gmail.com>
2014-06-30 18:47:46 +0200

    comments and fixes FilterParam.

--- ctt/FilterParam.h
+++ ctt/FilterParam.h
@@ -4,27 +4,49 @@
 #include <QScopedPointer>
 #include <QSharedPointer>
 #include <QWeakPointer>
+#include <QString>
+
+#include "FilterParam.h"
+
+class any; // TODO: Get a class
+
 namespace model {
-	namespace filter {
-		/**
-		 * Represents a parameter influencing the behaviour of a Filter.
-		 */
-		class FilterParam {
-		public:
-			typedef QScopedPointer<FilterParam> uptr;
-			typedef QSharedPointer<FilterParam> sptr;
-			typedef QWeakPointer<FilterParam> wptr;
+namespace filter {
 
-			any getValue();
-			/**
-			 * Creates a new FilterParam
-			 */
-			FilterParam(QString name, );
-		private:
-			QString name;
-		};
+/**
+ * Represents a parameter influencing a Filters behaviour.
+ */
+class FilterParam {
+public:
+	typedef QScopedPointer<FilterParam> uptr;
+	typedef QSharedPointer<FilterParam> sptr;
+	typedef QWeakPointer<FilterParam> wptr;
 
-	}  // namespace filter
+	/**
+	 * Creates a new FilterParam with the submitted name and value.
+	 * @param name the name of the parameter
+	 * @param value value of the parameter
+	 */
+	FilterParam(QString name, any value);
+
+	/**
+	 * Returns the value of the parameter.
+	 * @return any the value of the parameter
+	 */
+	any getValue() const;
+
+	/**
+	 * Returns the name of the parameter.
+	 * @return any the name of the parameter
+	 */
+	QString getName() const;
+
+	
+private:
+	QString name; /**<  The name of the parameter. */
+};
+
+}  // namespace filter
 }  // namespace model
 
 #endif  //_FILTERPARAM_H
