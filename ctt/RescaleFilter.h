183b5eef7e2b1748bb7bb5b0da65fab52901b24b
ugdsc <ugdsc@student.kit.edu>
2014-06-30 16:43:00 +0200

    Comment RescaleFilter

--- ctt/RescaleFilter.h
+++ ctt/RescaleFilter.h
@@ -11,12 +11,21 @@
 namespace model {
 namespace filter {
 
+	/**
+	 *	Provides a new version of the predecessors frame which has been rescaled.
+	 */
 class RescaleFilter : public Filter {
 public:
 	typedef QScopedPointer<RescaleFilter> uptr;
 	typedef QSharedPointer<RescaleFilter> sptr;
 	typedef QWeakPointer<RescaleFilter> wptr;
 
+	/**
+	*	Creates a new RescaleFilter object with a given previous module.
+	*
+	*	@param previous The previous module of this filter.
+	*/
+	RescaleFilter(Module previous);
 	QString getName();
 	Frame getFrame(int frameNumber);
 	Memento getMemento();
