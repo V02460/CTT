4cd927afa850cdfe177d2a84cea927684bd21ac1
ugdsc <ugdsc@student.kit.edu>
2014-06-30 19:34:38 +0200

    Project Saver and Loader commented

--- ctt/ProjectSaver.h
+++ ctt/ProjectSaver.h
@@ -11,13 +11,24 @@
 namespace model {
 namespace project {
 
+/**
+ * A project saver writes a file used to restore the project later. Therefor it takes mementos from every important savable object.
+ * Implementations of this interface may have their own conventions which type and structure of files they support.
+ */
 class ProjectSaver {
+
 public:
 	typedef QScopedPointer<ProjectSaver> uptr;
 	typedef QSharedPointer<ProjectSaver> sptr;
 	typedef QWeakPointer<ProjectSaver> wptr;
 
+	/**
+	 * Saves the current state of the project to a file.
+	 *
+	 * @throws IOException Is thrown if for any reason the file cant be written.
+	 */
 	void save(QDir path);
+
 private:
 	Saveable *toSave;
 };
