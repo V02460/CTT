ff15d01ce16d2ad9e5ca6ec9baa3e17362d69f48
ugdsc <ugdsc@student.kit.edu>
2014-06-30 17:11:50 +0200

    Comment HeatmapOverlay

--- ctt/HeatmapOverlay.h
+++ ctt/HeatmapOverlay.h
@@ -13,12 +13,22 @@ namespace model {
 namespace filter {
 namespace overlay {
 
+/**
+ *	Provides a new version of the predecessors frame where any PixelDiff was mixed graphically into it.
+ */
 class HeatmapOverlay : public ColoringOverlay {
 public:
 	typedef QScopedPointer<HeatmapOverlay> uptr;
 	typedef QSharedPointer<HeatmapOverlay> sptr;
 	typedef QWeakPointer<HeatmapOverlay> wptr;
 
+	/**
+	*	Creates a new HeatmapOverlay object with a given previous module and a PixelDiff function which should be displayed.
+	*
+	*	@param previous The previous module of this filter.
+	*	@param difference The difference that should be displayed.
+	*/
+	HeatmapOverlay(Module previous, model::difference::PixelDiff difference);
 	QString getName();
 	Frame getFrame(int frameNumber);
 	Memento getMemento();
