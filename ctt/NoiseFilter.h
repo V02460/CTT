47706f9bfc59c11fc44c16720b14a45c5e2e6564
ugdsc <ugdsc@student.kit.edu>
2014-06-30 16:41:41 +0200

    Comment NoiseFilter

--- ctt/NoiseFilter.h
+++ ctt/NoiseFilter.h
@@ -11,12 +11,21 @@
 namespace model {
 namespace filter {
 
+/**
+ *	Provides a new version of the predecessors frame where noise has been added.
+ */
 class NoiseFilter : public Filter {
 public:
 	typedef QScopedPointer<NoiseFilter> uptr;
 	typedef QSharedPointer<NoiseFilter> sptr;
 	typedef QWeakPointer<NoiseFilter> wptr;
 
+	/**
+	*	Creates a new NoiseFilter object with a given previous module.
+	*
+	*	@param previous The previous module of this filter.
+	*/
+	NoiseFilter(Module previous);
 	QString getName();
 	Frame getFrame(int frameNumber);
 	Memento getMemento();
