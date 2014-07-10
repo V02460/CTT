9bdf58c92dd7517b82d8ca07b411411d4a1c7aec
xTyrix <karsten.diekhoff@web.de>
2014-07-10 13:27:45 +0200

    projectloader trows added

--- ctt/ProjectLoader.cpp
+++ ctt/ProjectLoader.cpp
@@ -4,9 +4,10 @@ namespace controller {
 namespace project {
 
 using ::controller::project::Project;
+using ::exception::NotImplementedException;
 
 void ProjectLoader::restore(QDir path, Project project) {
-
+	throw NotImplementedException("The ProjectLoader Interface cannot restore a project.");
 }
 
 }  // namespace project
