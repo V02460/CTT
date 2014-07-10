246450d70ed3a2d3455b2b15a3acadf46c65edaa
xTyrix <karsten.diekhoff@web.de>
2014-07-10 13:55:07 +0200

    minor things

--- CTT/XMLLoader.cpp
+++ CTT/XMLLoader.cpp
@@ -3,5 +3,11 @@
 namespace controller {
 namespace project {
 
+using ::controller::project::Project;
+
+static void restore(QDir path, Project project){
+
+}
+
 }  // namespace project
 }  // namespace controller
