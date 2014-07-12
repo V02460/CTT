4681cd079679c7f293ba79af1d02441b617fff87
Kakistocrtor <jonaslkraemer@gmail.com>
2014-07-12 17:42:32 +0200

    I did nothing with this file but I have to commit it to switch to master.

---
+++ ctt/HistogramType.h
@@ -0,0 +1,25 @@
+#ifndef _HISTOGRAMTYPE_H
+#define _HISTOGRAMTYPE_H
+
+namespace model {
+namespace frame {
+namespace histogram {
+
+/**
+ * Labels for the channels histograms describe the intensity distribution of.
+ *
+ */
+enum HistogramType {
+    Red,
+    Green,
+    Blue,
+    Luminance,
+    Hue,
+    Saturation
+};
+
+}  // namespace histogram
+}  // namespace frame
+}  // namespace model
+
+#endif  //_HISTOGRAMTYPE_H
