8ccc121c8af29d14ea634b29472799b5b80ec2b5
Kai Hiller <kai.hiller@student.kit.edu>
2014-07-13 04:17:15 +0200

    Implement Filter

--- ctt/Filter.cpp
+++ ctt/Filter.cpp
@@ -1,7 +1,7 @@
 #include "Filter.h"
-#include "Module.h"
-#include "Frame.h"
-#include "Memento.h"
+
+#include "IllegalArgumentException.h"
+#include "AccessToDummyException.h"
 
 namespace model {
 namespace filter {
@@ -9,9 +9,10 @@ namespace filter {
 using ::model::frame::Frame;
 using ::model::Module;
 using ::model::saveable::Memento;
+using ::exception::AccessToDummyException;
+using ::exception::IllegalArgumentException;
 
-Filter::Filter(Module::sptr predecessor) {
-
+Filter::Filter(Module::sptr predecessor) : predecessor(predecessor), parameters(), intervals() {
 }
 
 Filter::~Filter() {
@@ -19,27 +20,62 @@ Filter::~Filter() {
 }
 
 QList<FilterParam> Filter::getParams() const {
+    if (isDummy()) {
+        throw new AccessToDummyException();
+    }
 
+    return parameters.values();
 }
 
 void Filter::setParam(FilterParam parameter) {
+    if (isDummy()) {
+        throw new AccessToDummyException();
+    }
 
+    parameters.insert(parameter.getName(), parameter);
 }
 
-void Filter::setPreviousModule(Module::sptr previous) {
+void Filter::setPreviousModule(Module::sptr predecessor) {
+    if (isDummy()) {
+        throw new AccessToDummyException();
+    }
+    if (predecessor.isNull()) {
+        throw new IllegalArgumentException("Predecessor must not be null.");
+    }
 
+    this->predecessor = predecessor;
 }
 
 void Filter::activate(UIntegerInterval interval) {
+    if (isDummy()) {
+        throw new AccessToDummyException();
+    }
 
+    intervals.activate(interval);
 }
 
 void Filter::deactivate(UIntegerInterval interval) {
+    if (isDummy()) {
+        throw new AccessToDummyException();
+    }
 
+    intervals.deactivate(interval);
+}
+
+QList<UIntegerInterval> Filter::getListOfActiveIntervals() {
+    if (isDummy()) {
+        throw new AccessToDummyException();
+    }
+
+    return intervals.getIntervalList();
 }
 
 unsigned int Filter::getFrameCount() const {
+    if (isDummy()) {
+        throw new AccessToDummyException();
+    }
 
+    predecessor->getFrameCount();
 }
 
 }  // namespace filter
