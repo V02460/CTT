f666295c826106b3d78ccdd99db5bf9df81092af
ugdsc <ugdsc@student.kit.edu>
2014-08-16 23:37:38 +0200

    Add localiztation file

---
+++ CTT/ctt_en.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>cttClass</name>
+    <message>
+        <location filename="ctt.ui" line="14"/>
+        <source>ctt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ctt.ui" line="21"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>view::ListedPushButton</name>
+    <message>
+        <location filename="ListedPushButton.cpp" line="15"/>
+        <source>NO_VIDEO_SPECIFIED</source>
+        <comment>Missing video for pushbutton</comment>
+        <translation>Error while transfering video</translation>
+    </message>
+</context>
+</TS>
