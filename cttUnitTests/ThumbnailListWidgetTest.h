c05a24233e5364353bfc9790831daa046bdd5f72
ugdsc <ugdsc@student.kit.edu>
2014-08-17 10:44:03 +0200

    Add test widget for the ThumbnailListWidget

---
+++ cttUnitTests/ThumbnailListWidgetTest.h
@@ -0,0 +1,15 @@
+#ifndef _THUMBNAILLISTWIDGETTEST
+#define _THUMBNAILLISTWIDGETTEST
+
+#include <QWidget>
+
+class ThumbnailListWidgetTest : public QWidget {
+	Q_OBJECT
+public:
+	ThumbnailListWidgetTest(QWidget *parent = 0);
+
+private:
+	void setupUi();
+};
+
+#endif //_THUMBNAILLISTWIDGETTEST
