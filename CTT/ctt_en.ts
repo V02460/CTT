31062d89efea9016cb3c501e65a7626028206cf9
ugdsc <ugdsc@student.kit.edu>
2014-08-18 19:46:31 +0200

    Update localization file

--- CTT/ctt_en.ts
+++ CTT/ctt_en.ts
@@ -17,10 +17,83 @@
 <context>
     <name>view::ListedPushButton</name>
     <message>
-        <location filename="ListedPushButton.cpp" line="15"/>
         <source>NO_VIDEO_SPECIFIED</source>
         <comment>Missing video for pushbutton</comment>
-        <translation>Error while transfering video</translation>
+        <translation type="vanished">Error while transfering video</translation>
+    </message>
+    <message>
+        <location filename="ListedPushButton.cpp" line="15"/>
+        <source>NO_VIDEO_SPECIFIED</source>
+        <translation type="unfinished">Error while transfering video</translation>
+    </message>
+</context>
+<context>
+    <name>view::PlayerFunctions</name>
+    <message>
+        <location filename="PlayerFunctions.cpp" line="34"/>
+        <source>PREVIOUS_FRAME</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlayerFunctions.cpp" line="40"/>
+        <source>NEXT_FRAME</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlayerFunctions.cpp" line="53"/>
+        <source>RESET_TO_DEFAULT_FPS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlayerFunctions.cpp" line="55"/>
+        <source>DEFAULT_FPS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlayerFunctions.cpp" line="62"/>
+        <source>PLAY</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlayerFunctions.cpp" line="66"/>
+        <source>PAUSE</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>view::ThumbnailListWidget</name>
+    <message>
+        <location filename="ThumbnailListWidget.cpp" line="43"/>
+        <source>ADD_VIDEO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ThumbnailListWidget.cpp" line="97"/>
+        <source>OPEN_VIDEO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ThumbnailListWidget.cpp" line="97"/>
+        <source>ALL_FILES (*.*)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>view::VideoProcessingWidget</name>
+    <message>
+        <location filename="VideoProcessingWidget.cpp" line="29"/>
+        <source>SAVE_VIDEO_DIALOG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="VideoProcessingWidget.cpp" line="41"/>
+        <source>USE_VIDEO_FOR_ANALYSIS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="VideoProcessingWidget.cpp" line="47"/>
+        <source>SAVE_VIDEO</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
