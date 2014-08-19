cbb85dd93683368539991804832a93cbd2739347
ugdsc <ugdsc@student.kit.edu>
2014-08-19 04:36:24 +0200

    Add ExtendedVideoListController 's header

---
+++ CTT/ExtendedVideoListController.h
@@ -0,0 +1,28 @@
+#ifndef _EXTENDEDVIDEOLISTCONTROLLER
+#define _EXTENDEDVIDEOLISTCONTROLLER
+
+#include "VideoListController.h"
+
+#include "FilteredVideo.h"
+#include "Player.h"
+
+namespace controller {
+	class ExtendedVideoListController : public controller::VideoListController {
+	public:
+		ExtendedVideoListController(model::saveable::SaveableList<model::video::Video>::sptr baseVideos,
+			model::saveable::SaveableList<model::filter::FilteredVideo>::sptr filteredVideos,
+			model::saveable::SaveableList<model::player::Player>::sptr player);
+
+		public slots:
+		void addVideo(QString path);
+		void addVideo(model::video::Video::sptr video);
+		void removeVideo(int index);
+		void removeVideo(const model::video::Video &video);
+
+	private:
+		model::saveable::SaveableList<model::filter::FilteredVideo>::sptr filteredVideos;
+		model::saveable::SaveableList<model::player::Player>::sptr player;
+	};
+}
+
+#endif //_EXTENDEDVIDEOLISTCONTROLLER
