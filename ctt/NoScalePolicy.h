2d9d058c7c3f5fec15a9380a5d1977278728bb3e
ugdsc <ugdsc@student.kit.edu>
2014-08-22 15:38:50 +0200

    Add pointer typedefs to NoScalePolicy and VideoDisplayPolicy

--- ctt/NoScalePolicy.h
+++ ctt/NoScalePolicy.h
@@ -1,12 +1,20 @@
 #ifndef _NOSCALEPOLICY
 #define _NOSCALEPOLICY
 
+#include <QScopedPointer>
+#include <QSharedPointer>
+#include <QWeakPointer>
+
 #include "VideoDisplayPolicy.h"
 
 namespace view {
 
 class NoScalePolicy : public VideoDisplayPolicy {
 public:
+	typedef QScopedPointer<NoScalePolicy> uptr;
+	typedef QSharedPointer<NoScalePolicy> sptr;
+	typedef QWeakPointer<NoScalePolicy> wptr;
+
 	NoScalePolicy();
 	virtual QRect getViewportSize(QSize displaySize, QSize videoSize) Q_DECL_OVERRIDE;
 };
--- ctt/VideoDisplayPolicy.h
+++ ctt/VideoDisplayPolicy.h
@@ -1,12 +1,19 @@
 #ifndef _VIDEODISPLAYPOLICY
 #define _VIDEODISPLAYPOLICY
 
+#include <QScopedPointer>
+#include <QSharedPointer>
+#include <QWeakPointer>
 #include <QRect>
 
 namespace view {
 
 class VideoDisplayPolicy {
 public:
+	typedef QScopedPointer<VideoDisplayPolicy> uptr;
+	typedef QSharedPointer<VideoDisplayPolicy> sptr;
+	typedef QWeakPointer<VideoDisplayPolicy> wptr;
+
 	virtual QRect getViewportSize(QSize displaySize, QSize videoSize) = 0;
 
 private:
