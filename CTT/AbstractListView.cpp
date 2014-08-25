ea3ffe5565949bc2607718fe1523846592cfd574
ugdsc <ugdsc@student.kit.edu>
2014-08-25 20:29:31 +0200

    Implement AbstractListView

--- CTT/AbstractListView.cpp
+++ CTT/AbstractListView.cpp
@@ -1,5 +1,58 @@
 #include "AbstractListView.h"
 
-namespace view {
+#include <QTreeWidgetItem>
+#include <QBoxLayout>
+#include <QLabel>
 
+#include "ListedPushButton.h"
+
+namespace view {
+AbstractListView::AbstractListView(QWidget *parent) : QTreeWidget(parent) {
+	items = QList<AbstractListViewItem::sptr>();
+	setIndentation(0);
+	//setItemsExpandable(true);
+	setRootIsDecorated(true);
+}
+
+void AbstractListView::removeAllItems() {
+	while (!items.isEmpty()) {
+		QTreeWidgetItem *item = takeTopLevelItem(0);
+		delete item;
+		items.removeFirst();
+	}
+}
+
+void AbstractListView::setupUi() {
+	for (int i = 0; i < items.size(); i++) {
+		//Create and set header
+		QTreeWidgetItem *item = new QTreeWidgetItem();
+		addTopLevelItem(item);
+
+		QWidget *itemWidget = new QWidget();
+		QHBoxLayout *itemWidgetLayout = new QHBoxLayout();
+
+		QLabel *identifierLabel = new QLabel(items.at(i)->getIndentifier());
+		itemWidgetLayout->addWidget(identifierLabel);
+		itemWidgetLayout->addStretch();
+
+		//TODO Add icon
+		ListedPushButton *removeButton = new ListedPushButton(i);
+		removeButton->setText(tr("REMOVE"));
+		itemWidgetLayout->addWidget(removeButton);
+		QObject::connect(removeButton, SIGNAL(clicked(bool, int)), this, SLOT(buttonRemoveClicked(bool, int)));
+
+		itemWidget->setLayout(itemWidgetLayout);
+		setItemWidget(item, 0, itemWidget);
+
+		//Create and set child
+		QTreeWidgetItem *childItem = new QTreeWidgetItem();
+		childItem->setDisabled(true);
+		item->addChild(childItem);
+		setItemWidget(childItem, 0, items.at(i).data());
+	}
+}
+
+void AbstractListView::buttonRemoveClicked(bool checked, int id) {
+	emit elementRemoved(id);
+}
 }  // namespace view
--- ctt/AbstractListView.h
+++ ctt/AbstractListView.h
@@ -4,9 +4,11 @@
 #include <QScopedPointer>
 #include <QSharedPointer>
 #include <QWeakPointer>
+#include <QTreeWidget>
 
 #include "Observer.h"
 #include "Observable.h"
+#include "AbstractListViewItem.h"
 
 namespace view {
 
@@ -15,12 +17,23 @@ namespace view {
  * It stands basically for some kind of list and displays all the elements of that list in a way that is concretized in
  * the specific implementation.
  */
-class AbstractListView : public ::model::Observer, public ::model::Observable {
+class AbstractListView : public ::model::Observer, public ::model::Observable, public QTreeWidget {
+	Q_OBJECT
 public:
     typedef QScopedPointer<AbstractListView> uptr;
     typedef QSharedPointer<AbstractListView> sptr;
     typedef QWeakPointer<AbstractListView> wptr;
 
+signals:
+	void elementRemoved(int id);
+protected:
+	AbstractListView(QWidget *parent = 0);
+	void removeAllItems();
+	void setupUi();
+
+	QList<AbstractListViewItem::sptr> items;
+protected slots:
+	void buttonRemoveClicked(bool checked, int id);
 private:
     //SaveableList diplayableComponents; /**< The list over the components that should be displayed by the list view */
 };
