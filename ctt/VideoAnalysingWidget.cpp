b7d4f58c5d14a223c2d97ea9df8e2f72e24d6087
ugdsc <ugdsc@student.kit.edu>
2014-08-29 10:29:20 +0200

    Implement base version of VideoAnalysingWidget (more work to be done)

--- ctt/VideoAnalysingWidget.cpp
+++ ctt/VideoAnalysingWidget.cpp
@@ -1,17 +1,64 @@
 #include "VideoAnalysingWidget.h"
 
+#include <QBoxLayout>
+
 #include "NotImplementedException.h"
+#include "FilterFactory.h"
 
 namespace view {
 
 using ::exception::NotImplementedException;
+using ::controller::OverlayController;
+using ::model::player::VideoScrubber;
+using ::model::filter::FilterFactory;
+using ::model::video::Video;
 
-QString VideoAnalysingWidget::getOverlay() const {
-    throw new NotImplementedException();
+VideoAnalysingWidget::VideoAnalysingWidget(OverlayController::sptr overlayController, VideoScrubber::sptr scrubber,
+	QWidget *parent) : QWidget(parent) {
+	histWidget = new HistogramWidget(scrubber, this);
+	videoWidget = new VideoWidget(scrubber);
+	metadataWidget = new FrameMetadataWidget(scrubber, this);
+
+	setupUi();
+}
+
+void VideoAnalysingWidget::setupUi() {
+	QVBoxLayout *mainLayout = new QVBoxLayout();
+
+	QWidget *videoWidgetWrapper = QWidget::createWindowContainer(videoWidget, this, Qt::Widget);
+	mainLayout->addWidget(videoWidgetWrapper);
+
+	QHBoxLayout *informationLayout = new QHBoxLayout();
+	comboboxOverlay = new QComboBox(this);
+	comboboxOverlay->addItem(tr("NO_OVERLAY"));
+	comboboxOverlay->addItems(FilterFactory::getAllOverlayIDs());
+	comboboxOverlay->setCurrentIndex(0);
+	QObject::connect(comboboxOverlay, SIGNAL(currentIndexChanged(int)),
+		this, SLOT(comboboxOverlayCurrentIndexChanged(int)));
+	informationLayout->addWidget(comboboxOverlay);
+
+	informationLayout->addWidget(histWidget);
+
+	informationLayout->addWidget(metadataWidget);
+
+	mainLayout->addLayout(informationLayout);
+
+	setLayout(mainLayout);
 }
 
 void VideoAnalysingWidget::comboboxOverlayCurrentIndexChanged(int index) {
-    throw new NotImplementedException();
+	emit overlayRemoved(0);
+
+	if (index > 0) {
+		QString overlayId = FilterFactory::getAllOverlayIDs().at(index - 1);
+
+		if (index == 1) {
+			//TODO Request video for difference
+			emit overlayAdded(overlayId, Video::sptr());
+		} else {
+			emit overlayAdded(overlayId);
+		}
+	}
 }
 
 }  // namespace view
