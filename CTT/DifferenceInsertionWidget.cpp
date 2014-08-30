eb4970bd6a2e7d92a861b520f2e2ab132ffe0e0a
ugdsc <ugdsc@student.kit.edu>
2014-08-30 13:17:01 +0200

    Add dummy DifferenceInsertionWidget

---
+++ CTT/DifferenceInsertionWidget.cpp
@@ -0,0 +1,14 @@
+#include "DifferenceInsertionWidget.h"
+
+using ::controller::DifferenceController;
+
+namespace view {
+	DifferenceInsertionWidget::DifferenceInsertionWidget(DifferenceController::sptr differenceController,
+		AnalysingOrderingWidget::sptr orderingWidget, QWidget *parent) {
+
+	}
+
+	void DifferenceInsertionWidget::update() {
+
+	}
+}
