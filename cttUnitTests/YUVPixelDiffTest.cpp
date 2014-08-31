9d64ccdf4433978db70fcec00068518841694e33
Kai Hiller <kai.hiller@student.kit.edu>
2014-09-01 00:14:17 +0200

    Add HSLPixelDiff and YUVPixelDiff test cases

---
+++ cttUnitTests/YUVPixelDiffTest.cpp
@@ -0,0 +1,46 @@
+#include "YUVPixelDiffTest.h"
+
+#include "YUVPixelDiff.h"
+#include "IllegalArgumentException.h"
+
+namespace model {
+namespace difference {
+
+using ::model::video::YUVDataVideo;
+using ::exception::IllegalArgumentException;
+
+void YUVPixelDiffTest::initTestCase() {
+    testContext.reset(new QOpenGLContext());
+    testContext->create();
+
+    surface.create();
+
+    testContext->makeCurrent(&surface);
+
+    video1.reset(new YUVDataVideo("Resources/Videos/YUV422/squirrel-720x576-422P.yuv",
+        QSize(720, 576),
+        24,
+        YUVType::YUV422,
+        testContext));
+
+    video2.reset(new YUVDataVideo("Resources/Videos/YUV444/squirrel-720x576-444P.yuv",
+        QSize(720, 576),
+        24,
+        YUVType::YUV444,
+        testContext));
+}
+
+void YUVPixelDiffTest::testRun() {
+    YUVPixelDiff diff(video1, video2);
+    diff.getDiff(5);
+    diff.getPixelDiff(8);
+}
+
+void YUVPixelDiffTest::badArgs() {
+    YUVPixelDiff diff(video1, video2);
+    QVERIFY_EXCEPTION_THROWN(diff.getDiff(-6), IllegalArgumentException);
+    QVERIFY_EXCEPTION_THROWN(diff.getPixelDiff(666), IllegalArgumentException);
+}
+
+}  // namespace difference
+}  // namespace model
