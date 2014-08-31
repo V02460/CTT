0c4f65f1acdac3ac175c860c33bc73213cb7055d
fernel <n.fernengel@gmail.com>
2014-09-01 01:51:58 +0200

    implemented overlaycontroller (incomplete)

--- ctt/OverlayController.cpp
+++ ctt/OverlayController.cpp
@@ -1,27 +1,49 @@
 #include "OverlayController.h"
+#include "FilteredVideo.h"
+#include "Module.h"
+#include "Operation.h"
+#include "OperationList.h"
+#include "FilterAddedOperation.h"
+#include "FilterRemovedOperation.h"
+#include "FilterMovedOperation.h"
+#include "FilterFactory.h"
+
 
 namespace controller {
 
-using ::model::video::Video;
+	using ::model::filter::FilteredVideo;
+	using ::model::Module;
+	using ::controller::operation::Operation;
+	using ::controller::operation::OperationList;
+	using ::controller::operation::FilterAddedOperation;
+	using ::controller::operation::FilterRemovedOperation;
+	using ::controller::operation::FilterMovedOperation;
+	using ::model::filter::FilterFactory;
 
-OverlayController::OverlayController(Video::sptr video) {
+	OverlayController::OverlayController(FilteredVideo::sptr video) : currentVideo(video){
 
-}
+	}
 
-OverlayController::~OverlayController() {
+	void OverlayController::insertOverlay(QString id) {
+		Module::sptr module;
+		if (currentVideo->getFilterCount() == 0) {
+			module = currentVideo->getBaseVideo();
+		}
+		else {
+			module = currentVideo->getFilterList().back();
+		}
+		OperationList::getInstance()->doOperation(QSharedPointer<Operation>(
+			new FilterAddedOperation(FilterFactory::createFilter(id, module), currentVideo)));
+	}
 
-}
+	void OverlayController::moveOverlay(int oldPos, int newPos) {
+		OperationList::getInstance()->doOperation(QSharedPointer<Operation>(
+			new FilterMovedOperation(oldPos, newPos, currentVideo)));
+	}
 
-void OverlayController::insertOverlay(QString id) {
-
-}
-
-void OverlayController::moveOverlay(int oldPos, int newPos) {
-
-}
-
-void OverlayController::removeOverlay(int pos) {
-
-}
+	void OverlayController::removeOverlay(int pos) {
+		OperationList::getInstance()->doOperation(QSharedPointer<Operation>(
+			new FilterRemovedOperation(pos, currentVideo)));
+	}
 
 }  // namespace controller
