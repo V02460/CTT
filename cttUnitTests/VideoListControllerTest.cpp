0750e82f637e4e237617f0362e9d70ee1d5b760c
fernel <n.fernengel@gmail.com>
2014-09-02 16:51:18 +0200

    fixed videlistcontrollertest

--- cttUnitTests/VideoListControllerTest.cpp
+++ cttUnitTests/VideoListControllerTest.cpp
@@ -1,20 +1,17 @@
 #include "VideoListControllerTest.h"
 #include "CustomTestingMacros.h"
 #include "VideoListController.h"
+#include "GlobalContext.h"
 
 #include "FilteredVideo.h"
 
 using model::video::YUVDataVideo;
 using model::filter::FilteredVideo;
 using controller::VideoListController;
+using model::GlobalContext;
 
 void VideoListControllerTest::initTestCase() {
-    testContext.reset(new QOpenGLContext());
-    testContext->create();
-
-    surface.create();
-
-    testContext->makeCurrent(&surface);
+	testContext = GlobalContext::get();
 
     video.reset(new YUVDataVideo("Resources/Videos/YUV422/squirrel-720x576-422P.yuv",
         QSize(720, 576),
@@ -34,7 +31,7 @@ void VideoListControllerTest::testRun() {
 	QVERIFY2(videoList->getSize() == 1, "Wrong number of videos in the videoList.");
 	QVERIFY2(videoList->get(0)->getBaseVideo() == filteredVideo, "The video in the videoList is not equal to the video that has been added.");
 	
-	videoListController.addVideo("Resources/Videos/YUV422/squirrel-720x576-422P.yuv", 720, 576, 24, YUVType::YUV422, video->getFrameCount()+1);
+	videoListController.addVideo("Resources/Videos/YUV422/squirrel-720x576-422P.yuv", 720, 576, 24, YUVType::YUV422);
 	QVERIFY2(videoList->getSize() == 2, "Wrong number of videos in the videoList.");
 	//QVERIFY2(videoList->get(1)->getBaseVideo() == video, "The video in the videoList is not equal to the video that has been added.");
 	
