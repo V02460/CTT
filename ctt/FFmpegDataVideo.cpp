d3b05ef34c6e7148abfd0dca0998693ad6ab9686
Kai Hiller <kai.hiller@student.kit.edu>
2014-10-01 20:28:15 +0200

    Reimplement FFmpegDataVideo

--- ctt/FFmpegDataVideo.cpp
+++ ctt/FFmpegDataVideo.cpp
@@ -1,206 +1,359 @@
 #include "FFmpegDataVideo.h"
 
-#include "NotImplementedException.h"
-#include "IOException.h"
+#define FFmpeg_PixelFormat PixelFormat
+#undef PixelFormat
+
+#include "GPUSurfaceShader.h"
+#include "GlobalContext.h"
+
 #include "FFmpegException.h"
+#include "NotImplementedException.h"
+#include "AccessToDummyException.h"
 
 namespace model {
 namespace video {
 
 using ::model::frame::Frame;
-using ::exception::NotImplementedException;
-using ::exception::IllegalStateException;
-using ::exception::IOException;
-using ::exception::FFmpegException;
+using ::model::frame::FrameMetadata;
+using ::model::saveable::Memento;
+using ::helper::GPUSurfaceShader;
+using ::model::GlobalContext;
 using ::model::saveable::Saveable;
+using ::exception::FFmpegException;
+using ::exception::NotImplementedException;
+using ::exception::AccessToDummyException;
 
-//TODO uziuf wie frameid initialisieren?
-FFmpegDataVideo::FFmpegDataVideo(QString path, QSharedPointer<QOpenGLContext> context) : FileVideo(path, context), frameId(0) {
+class FFmpegPacket {
+public:
+    typedef QScopedPointer<FFmpegPacket> uptr;
+    typedef QSharedPointer<FFmpegPacket> sptr;
+    typedef QWeakPointer<FFmpegPacket> wptr;
 
-	//TODO das muss nur einmal gemacht werden, nicht unbedingt in jedem konstruktoraufruf, muss noch angepasst werden, grad bin ich ja nur am testen und spielen
-	av_register_all();
+    /**
+     * Creates a FFmpegPacket and fills it with the next fitting data in the stream.
+     */
+    FFmpegPacket(AVFormatContext *avFormatContext, int streamNumber)
+            : avFormatContext(avFormatContext)
+            , streamNumber(streamNumber) {
+        retrieveNewData();
+    }
 
-	QByteArray rawPath = path.toLocal8Bit();
-	videoFormatContext = avformat_alloc_context();
+    ~FFmpegPacket() {
+        av_free_packet(&packet);
+    }
 
-	if (avformat_open_input(&videoFormatContext, rawPath.constData(), NULL, NULL) < 0)
-	{
-		throw IOException("FFmpeg couldn't open the file at \"" + path + "\".");
-	}
+    bool retrieveNewData() {
+        // get a packet from the correct stream
+        while (1) {
+            if (av_read_frame(avFormatContext, &packet)) {
+                //throw FFmpegException("Error or end of file reached.");
+                isEmptyFlag = true;
+                return false;
+            }
 
-	if (avformat_find_stream_info(videoFormatContext, NULL) < 0)
-	{
-		throw FFmpegException("FFmpeg couldn't find stream information in the file at \"" + path + "\".");
-	}
+            if (packet.stream_index == streamNumber) {
+                isEmptyFlag = false;
+                return true;
+            }
 
-	for (unsigned int i = 0; i < videoFormatContext->nb_streams; i++)
-	{
-		if (videoFormatContext->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
-			videoStreamNr = i;
-			break;
-		}
-	}
+            av_free_packet(&packet);
+        }
+    }
 
-	if (videoStreamNr == -1)
-	{
-		throw FFmpegException("FFmpeg couldn't find a video stream in the file at \"" + path + "\".");
-	}
+    bool isEmpty() const {
+        return isEmptyFlag;
+    }
 
-	//TODO muss der codeccontext irgendwie allokiert werden? ne oder? wäre vermutlich avcodec_alloc_context3(NULL)
-	codecContext = videoFormatContext->streams[videoStreamNr]->codec;
+    AVPacket *data() {
+        return &packet;
+    }
 
-	vCodec = avcodec_find_decoder(codecContext->codec_id);
-	if (vCodec == NULL) 
-	{
-		throw FFmpegException("FFmpeg couldn't find the codec necessary for decoding a video stream in the file at \"" + path + "\".");
-	}
+private:
+    Q_DISABLE_COPY(FFmpegPacket);
 
-	if (avcodec_open2(codecContext, vCodec, NULL) < 0)
-	{
-		throw FFmpegException("FFmpeg couldn't open the codec necessary to decode a video stream in the file at \"" + path + "\".");
-	}
+    AVFormatContext *avFormatContext;
+    int streamNumber;
 
-	//TODO ztdztd find this out somehow :(
-	length = 5000;// videoFormatContext->duration *;
+    AVPacket packet;
+    bool isEmptyFlag = true;
+};
+
+class FFmpegFrame {
+public:
+    typedef QScopedPointer<FFmpegFrame> uptr;
+    typedef QSharedPointer<FFmpegFrame> sptr;
+    typedef QWeakPointer<FFmpegFrame> wptr;
+
+    FFmpegFrame(AVFormatContext *avFormatContext, AVCodecContext *avCodecContext, int streamNr)
+            : avCodecContext(avCodecContext)
+            , isEmptyFlag(false) {
+        frame = av_frame_alloc();
+        if (!frame) {
+            throw FFmpegException("Could not allocate Frame.");
+        }
+
+        int got_picture = false;
+
+        while (!got_picture) {
+            FFmpegPacket packet(avFormatContext, streamNr);
+            if (packet.isEmpty()) {
+                isEmptyFlag = true;
+                break;
+            }
+
+            int nb_decompressedBytes = avcodec_decode_video2(avCodecContext, frame, &got_picture, packet.data());
+            if (nb_decompressedBytes < 0) {
+                throw FFmpegException("Failed to decode frame.");
+            }
+        } 
+    }
+
+    ~FFmpegFrame() {
+        av_frame_free(&frame);
+    }
+
+    QSize getSize() const {
+        return QSize(frame->width, frame->height);
+    }
+
+    int64_t getPTS() {
+        return frame->pkt_dts;
+    }
+
+    Surface::sptr getPlaneFromRawData(QSize planeSize, unsigned char *data, int linesize) {
+        unsigned char *planeBuffer;
+
+        auto plane = Surface::sptr::create(GlobalContext::get(),
+                                           planeSize,
+                                           reinterpret_cast<void**>(&planeBuffer),
+                                           QOpenGLTexture::UInt8,
+                                           QOpenGLTexture::Red,
+                                           QOpenGLTexture::R8_UNorm);
+
+        // row by row copy from ffmpeg to gpu
+        for (int i = 0; i < planeSize.height(); i++) {
+            memcpy(planeBuffer + i * planeSize.width(),
+                   data + i * linesize,
+                   planeSize.width());
+        }
+
+        plane->finishTextureUpload();
+
+        return plane;
+    }
+
+    Frame::sptr getGPUFrame() {
+        QSize yPlaneSize = getSize();
+        QSize chromaPlaneSize = yPlaneSize / 2;
+
+        Surface::sptr yPlane = getPlaneFromRawData(yPlaneSize, frame->data[0], frame->linesize[0]);
+        Surface::sptr uPlane = getPlaneFromRawData(chromaPlaneSize, frame->data[1], frame->linesize[1]);
+        Surface::sptr vPlane = getPlaneFromRawData(chromaPlaneSize, frame->data[2], frame->linesize[2]);
+
+        // convert YUV to RGB
+        // TODO implement support for hdtv
+        GPUSurfaceShader yuvToRGBConverter(":/Shader/Conversion/YUV420toRGBsdtv.fs", yPlane);
+
+        yuvToRGBConverter.setValue("uChannel", uPlane);
+        yuvToRGBConverter.setValue("vChannel", vPlane);
+        yuvToRGBConverter.invertY(true);
+
+        Surface::sptr rgbSurface = yuvToRGBConverter.run(yPlaneSize);
+
+        // Add metadata to Surface to make it a Frame
+        return Frame::sptr::create(rgbSurface, FrameMetadata(getSize()));
+    }
+
+    bool isEmpty() const {
+        return isEmptyFlag;
+    }
+
+private:
+    Q_DISABLE_COPY(FFmpegFrame);
+
+    AVCodecContext *avCodecContext;
+
+    AVFrame *frame;
+
+    bool isEmptyFlag;
+};
+
+
+FFmpegDataVideo::FFmpegDataVideo(QString path, QSharedPointer<QOpenGLContext> context)
+        : FileVideo(path, context)
+        , path(path)
+        ,frameCount(0) {
+    init();
 }
+
+void FFmpegDataVideo::init() {
+    loadVideo();
+
+    while (!FFmpegFrame(avFormatContext, avCodecContext, bestStreamNr).isEmpty()) {
+        frameCount++;
+    }
+
+    getFrame(0);
+}
+
+void FFmpegDataVideo::loadVideo() {
+    // be ready to handle all formats
+    av_register_all();
+
+    // create the context for receiving stream information
+    // avFormatContext = avformat_alloc_context();
+
+    // let FFmpeg allocate the context
+    avFormatContext = NULL;
+
+    // open file
+    if (avformat_open_input(&avFormatContext, path.toLatin1(), NULL, NULL) != 0) {
+        throw FFmpegException("Could not open video file '" + path + "'.");
+    }
+
+    // get infos needed to find the best video stream
+    int ret = avformat_find_stream_info(avFormatContext, NULL);
+    if (ret < 0) {
+        av_make_error_string(ffmpegErrorStr.data(), AV_ERROR_MAX_STRING_SIZE, ret);
+        throw FFmpegException("Could not find stream info: " + ffmpegErrorStr);
+    }
+
+    AVCodec *codec;
+    // find the best video stream
+    bestStreamNr = av_find_best_stream(avFormatContext, AVMediaType::AVMEDIA_TYPE_VIDEO, -1, -1, &codec, 0);
+    if (bestStreamNr < 0) {
+        switch (bestStreamNr) {
+            case AVERROR_STREAM_NOT_FOUND:
+                throw FFmpegException("Given files does not contain a video stream.");
+            case AVERROR_DECODER_NOT_FOUND:
+                throw FFmpegException("Did not find a decoder for any video stream.");
+            default:
+                throw FFmpegException("Unknown error occurred when calling av_find_best_stream.");
+        }
+    }
+
+    // get fps
+    fps = av_q2d(av_stream_get_r_frame_rate(avFormatContext->streams[bestStreamNr]));
+
+    avCodecContext = avcodec_alloc_context3(codec);
+    if (!avCodecContext) {
+        throw FFmpegException("Could not create codec context.");
+    }
+
+    if (avcodec_open2(avCodecContext, codec, NULL) != 0) {
+        throw FFmpegException("Could not open codec.");
+    }
+
+
+
+    // set function to choose output pixel format
+    //avCodecContext->get_format = pickPixelFormat;
+}
+
+FFmpegDataVideo::~FFmpegDataVideo() {
+    if (isDummy()) {
+        return;
+    }
+
+    avcodec_close(avCodecContext);
+    avcodec_free_context(&avCodecContext);
+
+    avformat_close_input(&avFormatContext);
+    avformat_free_context(avFormatContext);
+}
+
+Frame::sptr FFmpegDataVideo::getFrame(unsigned int frameNumber) const {
+    if (isDummy()) {
+        throw AccessToDummyException();
+    }
+    if (frameNumber >= getFrameCount()) {
+        throw IllegalArgumentException("Requested frame number " +
+                                       QString::number(frameNumber) +
+                                       " from video which only has " +
+                                       QString::number(getFrameCount()) +
+                                       " frames.");
+    }
+
+    // do we want the last frame again?
+    if (frameNumber == lastFrameNr) {
+        return lastFrame;
+    }
+
+    unsigned int decodeStart = 0;
+
+    // do we need a frame ahead?
+    if (lastFrameNr != -1 && frameNumber > lastFrameNr) {
+        decodeStart = lastFrameNr + 1;
+    } else {
+        // seek to begin
+        avformat_seek_file(avFormatContext,
+                           bestStreamNr,
+                           0,
+                           0,
+                           0,
+                           AVSEEK_FLAG_FRAME | AVSEEK_FLAG_ANY);
+    }
+
+    FFmpegFrame::sptr frame;
+
+    // TODO: don't bruteforce new frame position
+    for (unsigned int i = decodeStart; i <= frameNumber; i++) {
+        frame = FFmpegFrame::sptr::create(avFormatContext, avCodecContext, bestStreamNr);
+        if (frame->isEmpty()) { 
+            throw FFmpegException("Could not read frame " + QString::number(i) + " from stream.");
+        }
+    }
+
+    lastFrame = frame->getGPUFrame();
+    lastFrameNr = frameNumber;
+
+    return lastFrame;
+}
+
+const QString FFmpegDataVideo::kPathAttrStr = "path";
 
 VideoMetadata FFmpegDataVideo::getMetadata() const {
+    Frame::sptr probeFrame;
 
-	if (isDummy())
-	{
-		throw IllegalStateException("Tried to request metadata from a dummy ffmpeg video");
-	}
+    if (lastFrame.isNull()) {
+        probeFrame = getFrame(0);
+    } else {
+        probeFrame = lastFrame;
+    }
 
-	//TODO fps stimmen so nicht
-	VideoMetadata metadata(QSize(codecContext->width, codecContext->height), 1 / av_q2d(codecContext->time_base), length);
- 	return metadata;
+    return VideoMetadata(probeFrame->getSize(), fps, frameCount);
 }
 
-model::frame::Frame::sptr FFmpegDataVideo::getFrame(unsigned int frameNumber) const {
-
-	if (isDummy())
-	{
-		throw IllegalStateException("Tried to request a frame from a dummy ffmpeg video");
-	}
-
-	if (frameNumber >= length)
-	{
-		throw IllegalArgumentException("Tried to request the frame with the number " + QString::number(frameNumber) + " from a video having only  " + QString::number(getMetadata().getLength()) + " frames.");
-	}
-
-	AVFrame *frame;
-	frame = av_frame_alloc();
-
-	AVFrame *rgbFrame;
-	rgbFrame = av_frame_alloc();
-
-	uint8_t *buffer;
-	int bufferSize;
-
-	bufferSize = avpicture_get_size(PIX_FMT_RGB24, codecContext->width, codecContext->height);
-	buffer = (uint8_t *)av_malloc(bufferSize * sizeof(uint8_t));
-
-	avpicture_fill((AVPicture *)rgbFrame, buffer, PIX_FMT_RGB24, codecContext->width, codecContext->height);
-
-
-	int decodingSuccessfull = 0;
-
-	//TODO testen, wenns mal klappt vermutlich eigenes buffering einführen... muss man die internen ffmpeg buffer irgendwie flushen?
-
-	//seek to and read frame
-	int frame_delta = frameNumber - frameId;
-	if (frame_delta < 0 || frame_delta > 5)
-		if (av_seek_frame(videoFormatContext, videoStreamNr, frameNumber, AVSEEK_FLAG_BACKWARD) < 0)
-		{
-			throw new FFmpegException("Unable to seek to the requested location in the video.");
-		}
-	while (frameId != frameNumber)
-	{
-		//Read, maybe put this in a method
-		AVPacket packet;
-		av_init_packet(&packet);
-		while ((av_read_frame(videoFormatContext, &packet) >= 0) && !decodingSuccessfull) {
-			if (packet.stream_index == videoStreamNr) {
-				avcodec_decode_video2(codecContext, frame, &decodingSuccessfull, &packet);
-				if (decodingSuccessfull) {
-					frameId = packet.dts;
-				}
-			}
-		}
-		av_free_packet(&packet);
-	}
-
-	if (!decodingSuccessfull)
-	{
-		throw FFmpegException("FFmpeg couldn't decode the frame with the number " + QString::number(frameNumber) + " in the video stream .");
-	}
-
-	//TODO jdwfi the following sws stuff is for conversion to rgb format, this could of course be done on the gpu to be faster, but I don't know yet what the original data looks like, probably simple yuv
-	SwsContext *swsContext =
-		sws_getContext
-		(
-		codecContext->width,
-		codecContext->height,
-		codecContext->pix_fmt,
-		codecContext->width,
-		codecContext->height,
-		PIX_FMT_RGB24,
-		SWS_BILINEAR,
-		NULL,
-		NULL,
-		NULL
-		);
-
-	sws_scale
-		(
-		swsContext,
-		(uint8_t const * const *)frame->data,
-		frame->linesize,
-		0,
-		codecContext->height,
-		rgbFrame->data,
-		rgbFrame->linesize
-		);
-
-	QImage rgbImage(rgbFrame->data[0], codecContext->width, codecContext->height, QImage::Format_RGB888);
-
-	Frame::sptr result(new Frame(context, rgbImage));
-
-	av_free(buffer);
-	av_free(rgbFrame);
-	av_free(frame);
-
-	return result;
+unsigned int FFmpegDataVideo::getFrameCount() const {
+    return frameCount;
 }
 
-Saveable::SaveableType FFmpegDataVideo::getSaveableType() {
-    return SaveableType::fFmpegDataVideo;
+Memento FFmpegDataVideo::getMemento() const {
+    Memento memento;
+
+    memento.setString(kPathAttrStr, path);
+
+    return memento;
 }
 
-FFmpegDataVideo::~FFmpegDataVideo()
-{
-	avcodec_close(codecContext);
-	avformat_close_input(&videoFormatContext);
+void FFmpegDataVideo::restore(Memento memento) {
+    FileVideo::restore(memento);
+
+    path = memento.getString(kPathAttrStr);
+
+    init();
 }
 
-void FFmpegDataVideo::restore(::model::saveable::Memento memento)
-{
-	throw NotImplementedException();
+FFmpegDataVideo::FFmpegDataVideo() {
+    isDummyFlag = true;
 }
 
-unsigned int FFmpegDataVideo::getFrameCount() const
-{
-	if (isDummy())
-	{
-		throw IllegalStateException("Tried to request the frame count of a dummy ffmpeg video");
-	}
-	return length;
-}
-
-::model::saveable::Memento FFmpegDataVideo::getMemento() const
-{
-	throw NotImplementedException();
+Saveable::sptr FFmpegDataVideo::getDummy() {
+    return Saveable::sptr(new FFmpegDataVideo());
 }
 
 }  // namespace video
 }  // namespace model
+
+#define PixelFormat FFmpeg_PixelFormat
+#undef FFmpeg_PixelFormat
--- ctt/FFmpegDataVideo.h
+++ ctt/FFmpegDataVideo.h
@@ -9,68 +9,75 @@ extern "C"
 #include "libavutil\frame.h"
 }
 
+#define FFmpeg_PixelFormat PixelFormat
+#undef PixelFormat
+
 #include <QScopedPointer>
 #include <QSharedPointer>
 #include <QWeakPointer>
 
-#include "Video.h"
-#include "Frame.h"
-#include "Memento.h"
 #include "FileVideo.h"
+#include "Frame.h"
 
 namespace model {
 namespace video {
 
-/**
- * A Video read from a mpeg-2, mpeg-4 part 2 or mpeg-4 part 10 video file.
- */
 class FFmpegDataVideo : public FileVideo {
 public:
     typedef QScopedPointer<FFmpegDataVideo> uptr;
     typedef QSharedPointer<FFmpegDataVideo> sptr;
     typedef QWeakPointer<FFmpegDataVideo> wptr;
 
-    /**
-     * Creates a new FFmpegDataVideo representing the mpeg-2, mpeg-4 part 2 or mpeg-4 part 10 video at the submitted
-     * location in the file system.
-     *
-     * @param path the path to the video file which will be loaded
-     * @param context the context in which the video creates its frames
-     * @throws IOException if the file at the submitted location can't be accessed.
-     * @throws FFmpegException if ffmpeg isn't able to use the file as a video file it can open and decode
-     */
-	FFmpegDataVideo(QString path, QSharedPointer<QOpenGLContext> context);
+    FFmpegDataVideo(QString path, QSharedPointer<QOpenGLContext> context);
 
-	~FFmpegDataVideo();
+    ~FFmpegDataVideo();
 
     VideoMetadata getMetadata() const;
 
-    virtual model::frame::Frame::sptr getFrame(unsigned int frameNumber) const;
+    virtual model::frame::Frame::sptr getFrame(unsigned int frameNumber) const Q_DECL_OVERRIDE;
 
-    static Saveable::SaveableType getSaveableType();
-
-	virtual unsigned int getFrameCount() const;
-	virtual ::model::saveable::Memento getMemento() const;
-	virtual void restore(::model::saveable::Memento memento);
-	virtual SaveableType saveableType() const Q_DECL_OVERRIDE { return getSaveableType(); }
+    virtual unsigned int getFrameCount() const Q_DECL_OVERRIDE;
+    virtual ::model::saveable::Memento getMemento() const Q_DECL_OVERRIDE;
+    virtual void restore(::model::saveable::Memento memento) Q_DECL_OVERRIDE;
+    virtual SaveableType saveableType() const Q_DECL_OVERRIDE{return getSaveableType(); }
+    static ::model::saveable::Saveable::SaveableType getSaveableType() { return ::model::saveable::Saveable::fFmpegDataVideo; }
+    static ::model::saveable::Saveable::sptr getDummy();
 
 private:
-	//Information about the videoFile
-	AVFormatContext *videoFormatContext;
-	//Context of the codec used by the first video stream (the one that will be used)
-	AVCodecContext *codecContext;
-	//number of the first video stream (the one that will be used)
-	int videoStreamNr;
-	//the codec used to decode the video
-	AVCodec *vCodec;
-	//length in frames of the video
-	unsigned int length;
+    Q_DISABLE_COPY(FFmpegDataVideo);
 
-	mutable int64_t frameId;
+    /**
+     * Creates a dummy FFmpegDataVideo.
+     */
+    FFmpegDataVideo();
 
+    void init();
+    void loadVideo();
+    ::model::frame::Frame::sptr getNextFrame() const;
+
+    static const QString kPathAttrStr;
+
+    QString path;
+
+    AVFormatContext *avFormatContext = NULL;
+    AVCodecContext *avCodecContext = NULL;
+    QByteArray ffmpegErrorStr;
+
+    int bestStreamNr;
+
+    mutable ::model::frame::Frame::sptr lastFrame;
+    mutable unsigned int lastFrameNr = -1;
+
+    //int iFrameDistance = -1;
+    //QList<int> iFramePositions;
+    mutable unsigned int frameCount = -1;
+    double fps;
 };
 
 }  // namespace video
 }  // namespace model
+
+#define PixelFormat FFmpeg_PixelFormat
+#undef FFmpeg_PixelFormat
 
 #endif  //_FFMPEGDATAVIDEO_H
